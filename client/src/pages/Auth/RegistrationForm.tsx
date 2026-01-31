18805b6b589798586044bb93d52084aaefb49afa
Kacper Barabasz <barabasz64@gmail.com>
2026-01-31 15:18:15 +0100

    [Client] Create data storage logic for RegistrationForm

--- client/src/pages/Auth/RegistrationForm.tsx
+++ client/src/pages/Auth/RegistrationForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import BasicButton from '../../components/UI/BasicButton';
 import Input from '../../components/UI/Input';
 
@@ -6,14 +8,24 @@ interface RegistrationFormProps {
 }
 
 const RegistrationForm = ({ swapToLogin }: RegistrationFormProps) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
     <div className='flex flex-col gap-4 text-white'>
       <h1 className='text-white font-medium text-2xl text-center mb-5'>register</h1>
 
-      <Input type='text' inputId='username' labelText='Username' />
-      <Input type='email' inputId='email' labelText='Email' />
-      <Input type='password' inputId='password' labelText='Password' />
-      <Input type='password' inputId='confirmPassword' labelText='Confirm Password' />
+      <Input type='text' inputId='username' labelText='Username' onChange={e => setUsername(e.target.value)} />
+      <Input type='email' inputId='email' labelText='Email' onChange={e => setEmail(e.target.value)} />
+      <Input type='password' inputId='password' labelText='Password' onChange={e => setPassword(e.target.value)} />
+      <Input
+        type='password'
+        inputId='confirmPassword'
+        labelText='Confirm Password'
+        onChange={e => setConfirmPassword(e.target.value)}
+      />
 
       <BasicButton label='Register' onClick={() => {}} />
 
