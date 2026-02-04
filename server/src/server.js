0e0a91f9467407b92520c64e90fb97ab1dc4f21f
Kacper Barabasz <barabasz64@gmail.com>
2026-02-04 20:27:51 +0100

    [Server] Connect to MongoDB database using Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,23 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  'mongodb+srv://Table-Reservation-API:FeDiFAVDBVoRFCqk@cluster0.oehttvl.mongodb.net/?appName=TableReservation';
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose.connection.once('open', () => console.log('Mongoose connected successfully'));
+mongoose.connection.on('error', err => onsole.error(`Mongoose connection error: ${err}`));
+
+const startServer = async () => {
+  await mongoose.connect(MONGO_URL);
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
