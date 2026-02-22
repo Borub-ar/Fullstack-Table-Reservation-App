330fc7df6a1c9fd190fc8b8866ce4f869f96d40a
Kacper Barabasz <barabasz64@gmail.com>
2026-02-22 17:10:31 +0100

    [Server] Add additional tests for errorHandler middleware to cover AppError scenarios

--- server/src/middleware/errorHandler.test.ts
+++ server/src/middleware/errorHandler.test.ts
@@ -4,4 +4,16 @@ describe('errorHandler', () => {
   it('should return status code 500 and message "Something went wrong" when error is not an instance of AppError', () => {
     expect(true).toBe(true);
   });
+
+  it('should return status code and message from AppError', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should return fields from AppError if present', () => {
+    expect(true).toBe(true);
+  });
+
+  it("shouldn't return fields from AppError if not present", () => {
+    expect(true).toBe(true);
+  });
 });
